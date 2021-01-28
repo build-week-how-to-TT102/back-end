ef75c1dd7a3c78602a7ef050df966df398baccef
Mychael M <mychaelbmenges@gmail.com>
2021-01-27 19:30:58 -0700

    updated auth-model findById to not return hashed password

--- api/auth/auth-models.js
+++ api/auth/auth-models.js
@@ -5,7 +5,7 @@ function find() {
 }
 
 function findById(id) {
-  return db('users').where({id}).first()
+  return db('users').where({id}).select("username", "email", "id").first()
 }
 
 function findByUsername(username) {
