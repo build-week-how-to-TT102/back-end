a4bfae56a7d1bfcf9aa480b011e9ee0f4f30cac1
Mychael M <mychaelbmenges@gmail.com>
2021-01-27 19:10:40 -0700

    added auth models and route for register

---
+++ api/auth/auth-models.js
@@ -0,0 +1,25 @@
+const db = require('../../data/dbConfig')
+
+function find() {
+  db("users")
+}
+
+function findById(id) {
+  return db('users').where({id}).first()
+}
+
+function findByUsername(username) {
+  return db("users").where("users.username", username).first()
+}
+
+async function add(user) {
+ const [id] = await db('users').insert(user)
+ return findById(id)
+}
+
+module.exports = {
+  find,
+  findById,
+  findByUsername,
+  add,
+}
--- data/migrations/20210127183737_users.js
+++ data/migrations/20210127183737_users.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('users', users => {
+    users.increments()
+    users.string("username", 255).notNullable().unique()
+    users.string("password", 255).notNullable()
+    users.string("email", 255).notNullable()
+  })
 };
 
 exports.down = function(knex) {
-  
+  knex.schema.dropTableIfExists('users')
 };
