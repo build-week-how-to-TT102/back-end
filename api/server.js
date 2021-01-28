a334054b4c67b929a17ccde8b9cd02b0a52b3906
Mychael M <mychaelbmenges@gmail.com>
2021-01-27 19:21:22 -0700

    fixed issue with users migration and added rreturn on down function

--- api/server.js
+++ api/server.js
@@ -2,11 +2,15 @@ const express = require("express")
 const cors = require("cors")
 const helmet = require('helmet')
 
+const authRouter = require('./auth/auth-routers')
+
 const server = express()
 
 server.use(helmet())
 server.use(cors())
 server.use(express.json())
+
+server.use('/api/auth', authRouter)
 
 server.use("/", (req, res, next) => {
   res.status(200).json({
