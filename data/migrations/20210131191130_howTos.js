d89754d4d9733761624e12f5dff12e567a8f7b57
Mychael M <mychaelbmenges@gmail.com>
2021-02-01 17:34:46 -0700

    added steps table to db

--- data/migrations/20210131191130_howTos.js
+++ data/migrations/20210131191130_howTos.js
@@ -1,6 +1,6 @@
 
-exports.up = function(knex) {
-  return knex.schema.createTable('howtos', howto => {
+exports.up = async function(knex) {
+  await knex.schema.createTable('howtos', howto => {
     howto.increments()
     howto.string("title", 255).notNullable().unique()
     howto.string("description", 255).notNullable()
@@ -11,8 +11,21 @@ exports.up = function(knex) {
       .onUpdate("CASCADE")
       .onDelete("CASCADE")
   })
+
+  await knex.schema.createTable('steps', step => {
+    step.increments()
+    step.string("description").notNullable()
+    step.integer("step_number")
+    step.integer("howtoId")
+      .notNullable()
+      .references("id")
+      .inTable("howtos")
+      .onUpdate("CASCADE")
+      .onDelete("CASCADE")
+  })
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('howtos')
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('steps')
+  await knex.schema.dropTableIfExists('howtos')
 };
